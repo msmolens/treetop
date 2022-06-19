941b3525dcbeb2998286c35be7fa1ab653281e6b
Max Smolens <msmolens@gmail.com>
2022-06-19 17:12:53 -0400

    Use built-in Awaited type
    TypeScript 4.5 adds a new 'Awaited' utility type [1]. Remove the custom
    Awaited type in favor of the built-in type.
    
    [1] https://devblogs.microsoft.com/typescript/announcing-typescript-4-5/#awaited-type

--- src/treetop/types.ts
+++ src/treetop/types.ts
@@ -55,12 +55,3 @@ export type PreferenceValue =
   | string[]
   | number[]
   | boolean[];
-
-// Type to unwrap Promises. See:
-// - https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-1.html#recursive-conditional-types
-// - https://github.com/microsoft/TypeScript/pull/40002
-export type Awaited<T> = T extends null | undefined
-  ? T
-  : T extends PromiseLike<infer U>
-  ? Awaited<U>
-  : T;
