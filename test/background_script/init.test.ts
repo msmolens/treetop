eedb97ff4fe77196e64bf2e6350e1d36434f3c90
Max Smolens <msmolens@gmail.com>
2022-09-13 13:59:46 -0400

    Add event test for opening the welcome page
    Add event test for opening the welcome page when the extension is
    installed.

--- test/background_script/init.test.ts
+++ test/background_script/init.test.ts
@@ -1,3 +1,5 @@
+import { mockEvent } from 'mockzilla-webextension';
+
 import { init } from '@Treetop/background_script/init';
 import { createContextMenus } from '@Treetop/background_script/menus';
 import { openTreetop, openWelcome } from '@Treetop/background_script/open';
@@ -16,4 +18,18 @@ it('initializes extension', async () => {
 
   expect(setDefaultOptions).toHaveBeenCalledTimes(1);
   expect(createContextMenus).toHaveBeenCalledTimes(1);
+});
+
+it('opens the welcome page when the extension is installed', async () => {
+  const runtimeOnInstalled = mockEvent(mockBrowser.runtime.onInstalled);
+  mockBrowser.browserAction.onClicked.addListener.expect(openTreetop);
+
+  await init();
+
+  runtimeOnInstalled.emit({
+    reason: 'install',
+    temporary: false,
+  });
+
+  expect(openWelcome).toHaveBeenCalledTimes(1);
 });
