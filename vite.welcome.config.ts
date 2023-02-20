ef0db2f9c08a2d33ee0114f9ff1a75baa70eee3a
Max Smolens <msmolens@gmail.com>
2023-02-20 16:05:58 -0500

    Update Vite to 4.1

--- vite.welcome.config.ts
+++ vite.welcome.config.ts
@@ -1,23 +1,24 @@
-import { merge } from 'lodash';
+import merge from 'lodash/merge';
 import { resolve } from 'path';
 import { UserConfig, defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 import { developmentConfig } from './vite.common-development.config.js';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  /** @type {UserConfig} */
-  const commonConfig = {
-    plugins: [svelte()],
-    root: resolve(__dirname, 'src/background_script'),
+  const commonConfig: UserConfig = {
+    plugins: [
+      svelte({
+        preprocess: sveltePreprocess(),
+      }),
+    ],
+    root: resolve(__dirname, 'src/welcome'),
     build: {
-      lib: {
-        entry: 'main.ts',
-        name: 'background_script',
-        fileName: () => 'background_script.js',
-        formats: ['es'],
-      },
       outDir: resolve(__dirname, 'dist'),
+      rollupOptions: {
+        input: resolve(__dirname, 'src/welcome/welcome.html'),
+      },
     },
   };
 
