ff2a1b35ca33b1c06293dc42aed0b36a86142209
Max Smolens <msmolens@gmail.com>
2025-10-08 12:09:36 -0400

    Mock Web Animations API
    Avoid errors like `element.animate is not a function` when running
    Svelte tests that use transitions.
    
    See:
    - https://github.com/sveltejs/svelte/issues/16258
    - https://github.com/jsdom/jsdom/issues/3429

--- test/vitest-setup.ts
+++ test/vitest-setup.ts
@@ -1,1 +1,23 @@
+import { afterEach, beforeEach, vi } from 'vitest';
+
 import '@testing-library/jest-dom/vitest';
+
+// Mock Web Animations API.
+// See https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API.
+const mockAnimations = () => {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const savedAnimate = Element.prototype.animate;
+
+  beforeEach(() => {
+    Element.prototype.animate = vi.fn().mockReturnValue({
+      cancel: vi.fn(),
+      finished: Promise.resolve(),
+    });
+  });
+
+  afterEach(() => {
+    Element.prototype.animate = savedAnimate;
+  });
+};
+
+mockAnimations();
